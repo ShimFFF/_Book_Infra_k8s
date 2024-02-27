598462c601dcda8d255554c5d839099abc244401
ShimFFF <shimseawon1510@gmail.com>
2024-02-27 13:53:33 +0900

    :wrench: Chore: add vagrant setting for test invironment

---
+++ 2.2 베이그런트로 테스트 환경 구축하기/code/install_pkg.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# install packages
+yum install epel-release -y  # install epel-release (Extra Packages for Enterprise Linux)
+yum install vim-enhanced -y # install vim-enhanced (코드 하이라이팅)
